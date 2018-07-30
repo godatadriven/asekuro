93cd6bfe661f5f4f825505c04e94817dfc194d2e
koaning <vincentwarmerdam@gmail.com>
2018-07-30 21:53:32 +0200

    small changes

--- asekuro/commandline.py
+++ asekuro/commandline.py
@@ -63,6 +63,7 @@ def _testfile(nbpath):
     """
     Creates a path where to place a testing notebook.
     New file with have `-test.ipynb` at the end and is placed in `/tmp` dir.
+    We place it in tmp such that a docker container can write there.
     :param nbpath: Path to the notebook that needs to be tested.
     """
     return '/tmp/' + nbpath.replace(".ipynb", "-test.ipynb")
