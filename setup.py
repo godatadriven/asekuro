6bf0da01f5b62940b03293523bb71d967b9ce4a3
Barend Garvelink <barendgarvelink@godatadriven.com>
2018-08-10 13:36:19 +0200

    Import README.md as long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 import asekuro
 from setuptools import setup, find_packages
+from os import path
+
+
+def load_readme():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
     name='asekuro',
     version=asekuro.__version__,
     description='CLI util to deal with Jupyter Notebooks',
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
     author=['Vincent D. Warmerdam'],
     entry_points={
         'console_scripts': [
