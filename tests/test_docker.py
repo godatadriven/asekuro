d3ec64f2d07863c40fc2b254074ccb96f49c2d67
vincent <vincentwarmerdam@gmail.com>
2019-02-08 14:06:11 +0100

    clean up and add tests

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -4,5 +4,9 @@ import subprocess
 class TestDockerBuild:
 
     def test_docker_build_fails(self):
-        status = subprocess.call(['docker', 'build', '-t', 'asekurodock', '.'])
+        status = subprocess.call(['docker', 'build', '-t', 'asekurodock', '-f', 'tests/Dockerfile.test1', '.'])
         assert status == 2
+
+    def test_docker_build_is_fine(self):
+        status = subprocess.call(['docker', 'build', '-t', 'asekurodock', '-f', 'tests/Dockerfile.test2', '.'])
+        assert status == 0
